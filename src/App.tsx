74715f015210add898bf063959f8096c5914cfd7
Damian Celico <damiancelico@gmail.com>
2025-04-11 21:41:47 +0200

    creating youtube header with search bar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Logo from "./assets/Logo";
+import PageHeader from "./layouts/PageHeader";
+
+function App() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-gray-200", "hover:bg-gray-300"],
+      ghost: ["hover:bg-gray-100"],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
+
+export function Button({ variant, size, className, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={twMerge(buttonStyles({ variant, size }), className)}
+    />
+  );
+}
